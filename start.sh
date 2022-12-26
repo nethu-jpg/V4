a0417e09321de7bf1bfaec12316e75d9e38ffdbd
nethu-jpg <119031346+nethu-jpg@users.noreply.github.com>
2022-12-26 19:49:05 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/LazyDeveloperr/LazyPrincess.git /LazyPrincess
+  git clone https://github.com/nethu-jpg/V4.git /V4
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /LazyPrincess
+  git clone $UPSTREAM_REPO /V4
 fi
-cd /LazyPrincess
+cd /V4
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
